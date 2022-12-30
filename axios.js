92cbb35d57ef453a5a43ee5df7c893a1570e3ba7
Akkiro chudasama <chudasamaakshay45@gmail.com>
2022-12-30 19:30:54 +0530

    Added analytics and changed server

--- axios.js
+++ axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const baseURLLocal = 'http://192.168.43.172:4000';
 const baseURLLocal = 'http://192.168.43.45:4000';
-const baseURLHeroku = 'https://cryptic-castle-95582.herokuapp.com';
+const baseURLHeroku = 'https://rocket-server.onrender.com';
 
 const instance = axios.create({
   baseURL: baseURLHeroku
